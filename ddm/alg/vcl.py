988a0cc50513c1057f4c5387fbd7d0435cac3f51
TuanNguyen27 <anhtuan277@gmail.com>
2019-08-22 13:17:25 -0700

    Update vcl.py

--- ddm/alg/vcl.py
+++ ddm/alg/vcl.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import utils
-from cla_models_multihead import Vanilla_NN, MFVI_NN
+from cla_models_multihead import Vanilla_NN, MFVI_NN, CVI_NN
 
 def run_vcl(hidden_size, no_epochs, data_gen, coreset_method, coreset_size=0, batch_size=None, single_head=True):
     in_dim, out_dim = data_gen.get_dims()
