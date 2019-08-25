8ab7f2d0239dc24ca0d1aafef9044d1c48cfc902
TuanNguyen27 <anhtuan277@gmail.com>
2019-08-24 18:53:51 -0700

    Update utils.py

--- ddm/alg/utils.py
+++ ddm/alg/utils.py
@@ -38,7 +38,9 @@ def get_scores(model, x_testsets, y_testsets, x_coresets, y_coresets, hidden_siz
         x_test, y_test = x_testsets[i], y_testsets[i]
 
         pred = final_model.prediction_prob(x_test, head)
+        print(pred)
         pred_mean = np.mean(pred, axis=0)
+        print(pred_mean)
         pred_y = np.argmax(pred_mean, axis=1)
         y = np.argmax(y_test, axis=1)
         cur_acc = len(np.where((pred_y - y) == 0)[0]) * 1.0 / y.shape[0]
