bdb40246ac3668804ad37d5d4595b3e610b384a4
Tuan Nguyen <anhtuan277@gmail.com>
2019-09-05 22:55:11 -0700

    Update vcl.py

--- ddm/alg/vcl.py
+++ ddm/alg/vcl.py
@@ -35,7 +35,10 @@ def run_vcl(hidden_size, no_epochs, data_gen, coreset_method, coreset_size=0, ba
         mf_model = CVI_NN(in_dim, hidden_size, out_dim, x_train.shape[0], prev_means=mf_weights, prev_log_variances=mf_variances)
         mf_model.train(x_train, y_train, head, no_epochs, bsize)
         mf_weights, mf_variances = mf_model.create_weights()
-        # sess = mf_model.sess
+        sess = mf_model.sess
+        if not (mf_weights and mf_variances):
+            mf_weights = sess.run(mf_weights)
+            mf_variances = sess.run(mf_variances)
         # with sess.as_default():
         #     for weight in mf_weights:
         #         print_op = tf.print(weight)
