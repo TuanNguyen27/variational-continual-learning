8f0c90a15b3105e21cb133330071bd0055d337d3
Tuan Nguyen <anhtuan277@gmail.com>
2019-09-07 10:20:33 -0700

    Update cla_models_multihead.py

--- ddm/alg/cla_models_multihead.py
+++ ddm/alg/cla_models_multihead.py
@@ -500,7 +500,7 @@ class CVI_NN(Cla_NN):
         else:
             print("or here instead?")
             temp_sess = tf.compat.v1.Session()
-            #temp_sess.run(tf.compat.v1.global_variables_initializer())
+            temp_sess.run(tf.compat.v1.global_variables_initializer())
             prev_means = temp_sess.run(prev_means)
             prev_log_variances= temp_sess.run(prev_log_variances)
             new_priors_kernel = []
